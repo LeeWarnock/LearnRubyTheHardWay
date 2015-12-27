4ce42c25be997f726f66bc81ac389b7c191183c3
curtis.mckee <cmckee.dev@gmail.com>
2015-12-27 12:51:54 -0700

    Exercise 1

---
+++ ex1.rb
@@ -0,0 +1,16 @@
+# A Good First Program
+
+# puts "Hello World!!"
+# puts "Hello Again!"
+# puts "I like typing this."
+# puts "This is fun."
+# puts "Yay! This is fun."
+# puts "I'd much rather you not."
+# puts 'I "said" do not touch this.'
+puts "Merry Xmas, ya filthy animal."
+
+# Study Drills
+#
+# Make your script print another line.
+# Make your script print only one of the lines.
+# Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
