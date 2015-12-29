532427336fb20df21ffffb883eb1a2def3921699
cmckee <cmckee.dev@gmail.com>
2015-12-29 15:35:00 -0700

    Exercise 5

---
+++ ex5.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+# More Variables and Printing
+
+# my_name = 'Zed A. Shaw'
+name = 'Curtis Mckee'
+
+# my_age = 35 # Not a lie in 2009
+age = 27
+
+# my_height = 74 # Inches
+height = 70 * 2.54
+
+# my_weight = 180 # lbs
+weight = 125.0 * 0.453592
+
+# my_eyes = 'blue'
+eyes = 'blue'
+
+# my_teeth = 'white'
+teeth = 'getting whiter'
+
+# my_hair = 'brown'
+hair = 'light brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} cenitmeters tall."
+puts "He's #{weight} kilograms heavy."
+puts "Actually thats not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+# Study Drills
+#
+# Change all the variables so there is no my_ in front of each one. Make sure you change the name everywhere, not just where you used = to set them.
+# Try to write some variables that convert the inches and pounds to centimeters and kilograms. Do not just type in the measurements. Work out the math in Ruby.
