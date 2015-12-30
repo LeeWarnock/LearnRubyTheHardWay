56d9327b1f52b1b365cd626da9c53f8e5e89316c
cmckee <cmckee.dev@gmail.com>
2015-12-29 19:20:00 -0700

    Exercise 6

---
+++ ex6.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+# Strings and Text
+
+# assigns types_of_people variable to an integer
+types_of_people = 10
+
+# assigns x variable to the string
+# ONE
+x = "There are #{types_of_people} types of people."
+
+# assigns the binary variable to the string
+binary = "binary"
+# assigns the do_not variable to the string
+do_not = "Don't"
+
+# assigns y to the string
+# TWO
+y = "Those who know #{binary} and those who #{do_not}."
+
+# prints x variable to screen
+puts x
+# prints y variable to screen
+puts y
+
+# prints string to screen
+# THREE
+puts "I said: #{x}."
+# prints string to screen
+# FOUR
+puts "I also said: #{y}."
+# assigns the halarious variable to boolean 'False"
+halarious = false
+# assigns joke_evaluation to string
+# FIVE
+joke_evaluation = "Isn't that joke so funny?! #{halarious}"
+# prints joke_evaluation variable to screen
+puts "#{joke_evaluation}"
+
+# assigns w to string
+w =  "This is the left side of..."
+# assigns e wo a string
+e = "a string with a right side."
+
+# prints w variable then prints e variable
+puts w + e
+
+# Study drills
+#
+# Go through this program and write a comment above each line explaining it.
+# Find all the places where a string is put inside a string. There are four places.
+# Are you sure there are only four places? How do you know? Maybe I like lying.
+# Explain why adding the two strings w and e with + makes a longer string.
+# What happens when you change the strings to use ' (single-quote) instead of " (double-quote)? Do they still work? Try to guess why.
