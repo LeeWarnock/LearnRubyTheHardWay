410992a7cd4ae43c2616c7037e79aad181c3831a
cmckee <cmckee.dev@gmail.com>
2015-12-30 00:44:45 -0700

    Exercise 7

---
+++ ex7.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+# More Printing
+
+# prints a string to the screen
+puts "mary had a little lamb."
+# prints a string to the screen with a variable containing 'snow'
+puts "It's fleece was white as #{'snow'}."
+# prints a string to the screen
+puts "And everywhere that Merry went."
+# prints a string to the screen ten times
+puts "." *10 # What'd that do?
+
+# assigns end1-12 to variables that match each letter of the word 'CheeseBurger'
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+# prints the variables that contain the letters for 'Cheese' with no new line
+print end1 + end2 + end3 + end4 + end5 + end6
+# prints the varaibles that contain the letters for 'Burger' with new line.
+puts end7 + end8 + end9 + end10 + end11 + end12
+
+# Study drills
+#
+# Go back through and write a comment on what each line does.
+# Read each one backward or out loud to find your errors.
+# From now on, when you make mistakes, write down on a piece of paper what kind of mistake you made.
+# When you go to the next exercise, look at the mistakes you have made and try not to make them in this new one.
+# Remember that everyone makes mistakes. Programmers are like magicians who fool everyone into thinking they are perfect and never wrong, but it's all an act. They make mistakes all the time.
