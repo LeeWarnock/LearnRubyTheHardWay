428e6526d104c4764f09356980228cb52c64b5c7
curtis.mckee <curtis.mckee@mx.com>
2016-01-01 23:02:22 -0700

    Exercise 9

---
+++ ex9.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+#Printing, Printing, Printing
+
+# Here's some new strange stuff
+
+days = "Mon Tue Wen Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+There is something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
+# Study drills
+#
+# Check your work, write down your mistakes, try not to make them on the next exercise.
