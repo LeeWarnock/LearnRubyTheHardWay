518da752de43e71baaef6070e7752f5a747970bd
curtis.mckee <cmckee.dev@gmail.com>
2016-01-03 13:54:09 -0700

    Exercise 13 and Study Drills

---
+++ ex13-1.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+# Parameters, Unpacking, Variables
+# Study Drill Exercise
+
+name, greetings = ARGV
+
+puts "#{greetings}, #{name}!"
---
+++ ex13.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+# Parameters, Unpacking, Variables
+
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second varaible is: #{second}"
+puts "Your third variable is: #{third}"
+
+# Study drills
+#
+# Try giving fewer than three arguments to your script.
+# Write a script that has fewer arguments and one that has more. Make sure you give the unpacked variables good names.
+# Combine gets.chomp with ARGV to make a script that gets more input from a user.
