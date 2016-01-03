a2e82cfa137d5485636e0dd4532cdc43fb3117fa
curtis.mckee <curtis.mckee@mx.com>
2016-01-03 13:54:09 -0700

    Exercise 13 and Study Drills

---
+++ ex13-2.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+# Parameters, Unpacking, Variables
+# Study drills exercise
+
+list1, list2, list3, list4, list5 = ARGV
+
+puts "What kind of list is this?"
+list_name = STDIN.gets.chomp
+
+puts list_name
+puts "================="
+puts list1
+puts list2
+puts list3
+puts list4
+puts list5
